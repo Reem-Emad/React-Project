c482eb75a837f2d2094aac2446f4d33b6e75dc8e
asmaa <asmaaatahaa@gmail.com>
2019-03-29 12:50:50 +0200

    add json files

---
+++ src/Categories.js
@@ -0,0 +1,78 @@
+
+export default [
+    {
+        "Id": 1,
+        "Name": "Romance",
+    },
+    {
+        "Id": 2,
+        "Name": "Comics",
+    },
+    {
+        "Id": 3,
+        "Name": "Graphic Novel",
+    },
+    {
+        "Id": 4,
+        "Name": "Art",
+    },
+    {
+        "Id": 5,
+        "Name": "Fiction",
+    },
+    {
+        "Id": 6,
+        "Name": "CookBooks",
+    },
+    {
+        "Id": 7,
+        "Name": "Manga",
+    },
+    {
+        "Id": 8,
+        "Name": "Mystery",
+    },
+    {
+        "Id": 9,
+        "Name": "Nonfiction",
+    },
+    {
+        "Id": 10, "Name": "Crime",
+    },
+    {
+        "Id": 11,
+        "Name": "Horror",
+    },
+    {
+        "Id": 12,
+        "Name": "History",
+    },
+    {
+        "Id": 13,
+        "Name": "Biography",
+    },
+    {
+        "Id": 14,
+        "Name": "Chick Lit",
+    },
+    {
+        "Id": 15,
+        "Name": "CookBooks",
+    },
+    {
+        "Id": 16,
+        "Name": "Fantasy",
+    },
+    {
+        "Id": 17,
+        "Name": "Mystery",
+    },
+    {
+        "Id": 18,
+        "Name": "Science Fiction",
+    },
+    {
+        "Id": 19,
+        "Name": "Thriller",
+    },
+]
