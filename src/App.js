15f8778c81416a63cc0c8be3ca244d4f2e995416
Asma Soliman <msoliman.asmaa@gmail.com>
2019-04-02 11:22:45 +0200

    delete Author

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link,Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEdit} from '@fortawesome/free-solid-svg-icons'
-import {faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
 
 import HomePage from './components/SignIn-SignUp/Home_Page';
 import UserHome from './components/User/Home';
 import Categories from './components/Categories/Categories';
 import AllBooks from './components/Books/AllBooks';
 import Book from './components/Books/Book';
-import AuthorDetails from './components/Authors/Author';
+import AuthorDetails from './components/Authors/Authors';
 import BookDetailes from './components/Books/BookDetails';
 import Users from './Users';
 import Books from './Books';
-
 import AllAuthors from './components/Authors/AllAuthors';
 import AdminBooksList from './components/Books/admin-list';
 import AdminAuthorsList from './components/Authors/admin-list';
+import Authors from './Authors';
 
 
-library.add(faEdit)
-library.add(faTrashAlt)
+library.add(faEdit);
+library.add(faTrashAlt);
+library.add(faPlus);
 
 export const MyContext = React.createContext({ users:Users });
 class App extends React.PureComponent
@@ -30,6 +32,7 @@ class App extends React.PureComponent
     state={
        users:Users,        
        Books:Books,
+       Authors:Authors,
        loginedUser:{}
     }
     addNewUser=(newUser)=>{
@@ -39,12 +42,16 @@ class App extends React.PureComponent
     addLoginedUser=(loginedUser) =>{
         this.setState({loginedUser});
     }
+    updateAuthors=(updatedAuthors)=>{
+        this.setState({Authors:updatedAuthors});
+    }
     render()
     {
         const value={
             state: this.state,
             addNewUser: this.addNewUser,
-            addLoginedUser: this.addLoginedUser
+            addLoginedUser: this.addLoginedUser,
+            updateAuthors: this.updateAuthors,
         }
 
         return(
