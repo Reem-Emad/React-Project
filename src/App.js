43bb716b69dbe987162ac96856fabae29c461906
Reem <Reem@git.com>
2019-04-02 21:33:53 +0200

    solve route error agaaain

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends React.PureComponent {
                             <Route exact path="/" component={HomePage} />
                             {
                                Object.keys(this.state.loginedUser).length == 0 ?
-                              <Route exact path="/" component={HomePage} />
+                              <Route exact path="/"/>
                               :
                                 
                             <Switch>
