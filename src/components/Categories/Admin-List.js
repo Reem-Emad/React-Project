a2cab278e534aab60f6c031afa14f8a87def03c2
= <=>
2019-04-03 12:33:13 +0200

    final rehab commit

--- src/components/Categories/Admin-List.js
+++ src/components/Categories/Admin-List.js
@@ -3,23 +3,41 @@ import EditCard from './Edit-Card';
 import Books from '../../Books';
 import CategirosData from '../../Categories';
 import Navbar from '../Shared/Navbar';
-import {Row} from 'react-bootstrap';
+import { Row, Col, Card, Form, Modal, Button } from 'react-bootstrap';
 import { MyContext } from '../../App';
+import { timeout } from 'q';
+import AddCategory from './addCategort'
 
-class CategoriesAdminList extends React.Component{
-    
-    render(){
-         return(
-                <>
-          <Navbar></Navbar>
-            <Row className="no-gutters">
-                 {CategirosData.map(b=><EditCard key={b.Id}
-                 id={b.Id}
-                 name={b.Name}   />)}
-            </Row>
-            </>
-                    )
-                } 
+class CategoriesAdminList extends React.Component {
+
+     state = {
+          Categiros: CategirosData
+     }
+
+     DeleteHandelAdmin = (id) => {
+          const res = this.state.Categiros.filter(data =>
+               data.Id != id
+          )
+          this.setState({ Categiros: res })
+     }
+
+     render() {
+          return (
+               <>
+                    <Navbar></Navbar>
+                    <AddCategory />
+                    <Row className="no-gutters">
+                         {this.state.Categiros.map(b =>
+                              <EditCard key={b.Id}
+                                   id={b.Id}
+                                   name={b.Name}
+                                   delfun={this.DeleteHandelAdmin}
+                              />
+                         )}
+                    </Row>
+               </>
+          )
+     }
 }
 
 export default CategoriesAdminList;
--- src/components/Categories/Edit-Card.js
+++ src/components/Categories/Edit-Card.js
@@ -1,89 +1,50 @@
 import React from 'react';
-import { Card, Col, Modal, Button, Form } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CategirosData from '../../Categories';
+import EditCategory from './editCategory'
 
 class EditCard extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
         this.state = {
-            show: false,
-            smShow: false,
-            dataCat:CategirosData
+            showUpdate: false,
         };
-          this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
     }
 
-    handleClose() {
-        this.setState({ show: false });
+    ShowUpdate = () => {
+        this.setState({showUpdate: true})
     }
 
-    handleShow() {
-        this.setState({ show: true });
+    hideUpdate = () => {
+        this.setState({showUpdate: false})
     }
-    handelDelete=(id)=>{
-        var newArr = this.state.dataCat.filter(item => item.Id != this.props.Id)
-        this.setState({ smShow: true ,
-            dataCat: this.state.dataCat.splice(newArr, 1)
-        })
-        
-        console.log(this.props);
-     }
+
     render() {
-        let smClose = () => this.setState({ smShow: false });
         return (
             <>
+                <Row>
+                    <EditCategory 
+                        Id={this.props.id}
+                        Name={this.props.name}
+                        show={this.state.showUpdate}
+                        hideUpdate={this.hideUpdate}
+                        ShowUpdate={this.ShowUpdate}
+                    />
+                </Row>
+                
                 <Col className="m-3">
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: '18rem' }} onClick={this.ShowUpdate}>
                         <Card.Body>
                             <Card.Title>{this.props.name}
                                 <Card.Text className="float-right">
-                                    <FontAwesomeIcon className="mr-3" icon="edit" onClick={this.handleShow} />
-                                    <FontAwesomeIcon icon="trash-alt" onClick={() => {this.handelDelete(this.props.id)} } />
+                                    <FontAwesomeIcon className="mr-3" icon="edit" onClick={this.ShowUpdate} />
+                                    <FontAwesomeIcon icon="trash-alt" onClick={() => {this.props.delfun(this.props.id)} } />
                                 </Card.Text>
                             </Card.Title>
 
                         </Card.Body>
                     </Card>
                 </Col>
-
-                <Modal show={this.state.show} onHide={this.handleClose}>
-                    <Modal.Header>
-                        <Modal.Title>Edit Category</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Group >
-                                <Form.Label>Category</Form.Label>
-                                <Form.Control type="text" placeholder={this.props.category} />
-                            </Form.Group>
-                        </Form>
-
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                            Close
-                       </Button>
-                        <Button variant="primary" onClick={this.handleClose}>
-                            Save Changes
-                       </Button>
-                    </Modal.Footer>
-                </Modal>
-                <Modal
-                    size="sm"
-                    show={this.state.smShow}
-                    onHide={smClose}
-                    aria-labelledby="example-modal-sizes-title-sm">
-                     <Modal.Header closeButton>
-                        <Modal.Title id="example-modal-sizes-title-sm">
-                            Delete
-            </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        Category Deleted!
-          </Modal.Body>
-                </Modal>
             </>
 
         );
